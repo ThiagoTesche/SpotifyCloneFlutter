b70c35144cf0084763c052509b1e71940b2cdb6a
Thiago Tesche <thiago@Leonardos-iMac-Pro.local>
2023-02-07 13:35:33 -0300

    add bottomNav

---
+++ spotify_clone_flutter/lib/root_app.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+import 'package:spotify_clone_flutter/theme/colors.dart';
+
+class RootApp extends StatefulWidget {
+  const RootApp({super.key});
+
+  @override
+  State<RootApp> createState() => _RootAppState();
+}
+
+class _RootAppState extends State<RootApp> {
+  int activeTab = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: black,
+      bottomNavigationBar: getFooter(),
+      body: getBody(),
+    );
+  }
+
+  Widget getBody() {
+    return IndexedStack(
+      index: activeTab,
+      children: const <Widget>[
+        Center(
+          child: Text(
+            'Home',
+            style: TextStyle(
+                fontSize: 20, color: white, fontWeight: FontWeight.bold),
+          ),
+        ),
+        Center(
+          child: Text(
+            'Library',
+            style: TextStyle(
+                fontSize: 20, color: white, fontWeight: FontWeight.bold),
+          ),
+        ),
+        Center(
+          child: Text(
+            'Search',
+            style: TextStyle(
+                fontSize: 20, color: white, fontWeight: FontWeight.bold),
+          ),
+        ),
+        Center(
+          child: Text(
+            'Settings',
+            style: TextStyle(
+                fontSize: 20, color: white, fontWeight: FontWeight.bold),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget getFooter() {
+    List items = [
+      FeatherIcons.home,
+      FeatherIcons.book,
+      FeatherIcons.search,
+      FeatherIcons.settings
+    ];
+
+    return Container(
+        height: 80,
+        decoration: const BoxDecoration(color: black),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: List.generate(items.length, (index) {
+              return IconButton(
+                  icon: Icon(
+                    items[index],
+                    color: activeTab == index ? primary : white,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      activeTab = index;
+                    });
+                  });
+            }),
+          ),
+        ));
+  }
+}
